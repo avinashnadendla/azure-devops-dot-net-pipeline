a28f8e3243e1112aaed89543570f8af2839e84d6
AVINASH NADENDLA <E19CSE193@bennett.edu.in>
2021-07-25 10:34:56 +0000

    Updated webapp.ps1

--- webapp.ps1
+++ webapp.ps1
@@ -13,3 +13,12 @@ az role assignment create --assignee $svp --scope /subscriptions/$subid/resource
 az resource update --ids /subscriptions/$subid/resourceGroups/learning/providers/Microsoft.Web/sites/demowebapp2002/config/web --set properties.acrUseManagedIdentityCreds=True
 
 az webapp config container set --name demowebapp2002 --resource-group learning --docker-custom-image-name democon2002.azurecr.io/dotnetapp:latest --docker-registry-server-url https://democon2002.azurecr.io
+
+$cicd = $(az webapp deployment container config --enable-cd true --name demowebapp2002 --resource-group learning --query CI_CD_URL --output tsv)
+
+az acr webhook create --name appserviceCD --registry democon2002 --uri $cicd --actions push --scope democon2002.azurecr.io/dotnetapp:latest 
+
+
+$eventId=$(az acr webhook ping --name appserviceCD --registry democon2002 --query id --output tsv)
+
+az acr webhook list-events --name appserviceCD --registry democon2002 --query "[?id=='$eventId'].eventResponseMessage"
