2033566897224b0d83b9cc83c56b75fd48d80a87
AVINASH NADENDLA <E19CSE193@bennett.edu.in>
2021-07-25 08:50:28 +0000

    Updated webapp.ps1

--- webapp.ps1
+++ webapp.ps1
@@ -1,3 +1,3 @@
 az appservice plan create --name demowebappplan42002 --resource-group learning --is-linux
 
-az webapp create --resource-group learning --plan demowebappplan42002  --name demowebapp2002 --deployment-container-image-name niginx
+az webapp create --resource-group learning --plan demowebappplan42002  --name demowebapp2002 --deployment-container-image-name nginx
